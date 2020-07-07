2c6687ebf6f6723a1645a293805dc8eb1cd5d40f
itsRicmor <ricardojms1999@gmail.com>
2020-07-07 14:36:05 -0600

    NoteModel and Sql server configuration

---
+++ NotesApp/Data/ApplicationDbContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using NotesApp.Models;
+
+namespace NotesApp.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<NoteModel> Notes { get; set; }
+    }
+}
---
+++ NotesApp/Models/NoteModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace NotesApp.Models
+{
+    public class NoteModel
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+
+        public string Title { get; set; }
+
+        public string Description { get; set; }
+    }
+}
