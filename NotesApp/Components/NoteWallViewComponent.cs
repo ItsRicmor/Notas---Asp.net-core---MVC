71059cd982d1c7f54a871b5528e273ef655ffc9a
itsRicmor <ricardojms1999@gmail.com>
2020-07-07 22:47:16 -0600

    Add NoteWall View Component

---
+++ NotesApp/Components/NoteWallViewComponent.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using NotesApp.Core.Services;
+
+namespace NotesApp.Components
+{
+    [ViewComponent(Name = "NoteWall")]
+    public class NoteWallViewComponent : ViewComponent
+    {
+        private readonly INotesService _notesService;
+
+        public NoteWallViewComponent(INotesService notesService)
+        {
+            _notesService = notesService;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var notes = await _notesService.GetAllNotes();
+            return View(notes);
+        }
+    }
+}
--- NotesApp/Controllers/HomeController.cs
+++ NotesApp/Controllers/HomeController.cs
@@ -5,21 +5,27 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
-using NotesApp.Models;
+using NotesApp.Core.Models;
+using NotesApp.Core.Services;
 
 namespace NotesApp.Controllers
 {
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly INotesService _notesService;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger, INotesService notesService)
         {
             _logger = logger;
+            _notesService = notesService;
         }
 
+        // public async Task<IActionResult> Index()
         public IActionResult Index()
         {
+            // var note = new NoteModel { Title = "Cita", Description = "Cita de dientes" };
+            // await _notesService.CreateNote(note);
             return View();
         }
 
